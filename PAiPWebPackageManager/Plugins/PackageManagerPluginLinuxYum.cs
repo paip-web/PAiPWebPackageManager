55d53c77f5a44c758d67c676b8acc2d0b16465bd
Patryk Adamczyk <patrykadamczyk@patrykadamczyk.net>
2023-04-27 08:19:10 +0200

    Added Yum

---
+++ PAiPWebPackageManager/Plugins/PackageManagerPluginLinuxYum.cs
@@ -0,0 +1,109 @@
+﻿using System.Runtime.InteropServices;
+using PAiPWebPackageManager.PluginBase;
+using Spectre.Console;
+
+namespace PAiPWebPackageManager.Plugins;
+
+public class PackageManagerPluginLinuxYum: PluginBaseClass
+{
+    protected new PluginInformationRecord PluginMetadata = new PluginInformationRecord()
+    {
+        PluginCategoryType = PluginCategoryEnum.Operating_System_x_Linux,
+        PluginName = "Apk",
+        DoesNotWorkWithAdmin = false,
+        IsAdminNeeded = true,
+        RequiredCommands = new List<string>(new []{"yum", "yum-config-manager"}),
+        SupportedPlatforms = { OSPlatform.Linux },
+    };
+
+    public override bool IsSupported()
+    {
+        return CheckBasicRequirements();
+    }
+
+    public override bool IsInstallSupported()
+    {
+        return false;
+    }
+
+    public override bool IsPackageUriSupported(PackageUri packageUri)
+    {
+        return CheckIfPackageManagerUriIsSupported(packageUri, "yum");
+    }
+
+    public override bool IsPackageInstalled(PackageUri package)
+    {
+        return CheckExitCode(
+            ExecuteCommand($"yum list installed {package.GetPackage()}"),
+            new[] { 0 }
+        );
+    }
+
+    public override bool InstallPackage(PackageUri packageName)
+    {
+        return CheckExitCode(
+            ExecuteCommand($"yum install -y {packageName.GetPackage()}"),
+            new[] { 0 }
+        );
+    }
+
+    public override bool UpdatePackage(PackageUri packageName)
+    {
+        return CheckExitCode(
+            ExecuteCommand($"yum upgrade -y {packageName.GetPackage()}"),
+            new[] { 0 }
+        );
+    }
+
+    public override bool UninstallPackage(PackageUri packageName)
+    {
+        return CheckExitCode(
+            ExecuteCommand($"yum remove -y {packageName.GetPackage()}"),
+            new[] { 0 }
+        );
+    }
+
+    public override bool UpdatePackageDatabase()
+    {
+        // Not Supported / Not Needed
+        return true;
+    }
+
+    public override bool AddPackageRepositoryToDatabase(string repository)
+    {
+        return CheckExitCode(
+            ExecuteCommand($"yum-config-manager --add-repo {repository}"),
+            new[] { 0 }
+        );
+    }
+
+    public override bool RemovePackageRepositoryFromDatabase(string repository)
+    {
+        var resultBool = CheckExitCode(
+            ExecuteCommand($"yum-config-manager --disable {repository}"),
+            new[] { 0 }
+        );
+        
+        if (resultBool)
+        {
+            AnsiConsole.MarkupLineInterpolated(
+                $"[red]Removing repository works as just disabling repository. If you want to completely remove it. Remove this file: /etc/yum.repos.d/{repository}.repo[/red]"
+            );
+        }
+
+        return resultBool;
+    }
+
+    public override bool UpdateAllCurrentPackages()
+    {
+        return CheckExitCode(
+            ExecuteCommand("yum upgrade -y"),
+            new[] { 0 }
+        );
+    }
+
+    public override bool InstallPackageManager()
+    {
+        throw new NotSupportedException("This package manager does not support installation.");
+    }
+}
