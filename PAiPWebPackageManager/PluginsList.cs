577ee633e1e659ecbbd00d1689a57f90f8fce88d
Patryk Adamczyk <patrykadamczyk@patrykadamczyk.net>
2023-04-28 02:00:03 +0200

    Add PluginList Class (to Index all plugins in one specific place)

---
+++ PAiPWebPackageManager/PluginsList.cs
@@ -0,0 +1,32 @@
+﻿using PAiPWebPackageManager.PluginBase;
+using PAiPWebPackageManager.Plugins;
+
+namespace PAiPWebPackageManager;
+
+public static class PluginsList
+{
+    /// <summary>
+    /// List of all the plugins
+    /// </summary>
+    /// <returns>List of plugins</returns>
+    public static List<IPlugin> GetAllPlugins()
+    {
+        return new List<IPlugin>(new IPlugin[]
+        {
+            new PackageManagerPluginBrew(),
+            new PackageManagerPluginLinuxApk(),
+            new PackageManagerPluginLinuxDnf(),
+            new PackageManagerPluginLinuxFlatpak(),
+            new PackageManagerPluginLinuxPacman(),
+            new PackageManagerPluginLinuxPacstall(),
+            new PackageManagerPluginLinuxSnap(),
+            new PackageManagerPluginLinuxYum(),
+            new PackageManagerPluginMacOSBrewCask(),
+            new PackageManagerPluginMacOSMas(),
+            new PackageManagerPluginSpecialNix(),
+            new PackageManagerPluginWindowsChocolatey(),
+            new PackageManagerPluginWindowsScoop(),
+            new PackageManagerPluginWindowsWinGet(),
+        });
+    }
+}
