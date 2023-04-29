77ebecea4e2c356f6fa112b61f4c34cf7d6ab456
Patryk Adamczyk <patrykadamczyk@patrykadamczyk.net>
2023-04-29 14:29:03 +0200

    Add CliUtils

---
+++ PAiPWebPackageManager/Lib/CliUtils.cs
@@ -0,0 +1,17 @@
+﻿using System.CommandLine.Invocation;
+
+namespace PAiPWebPackageManager.Lib;
+
+public enum PwpmExitCode
+{
+    Success = 0,
+    Failure = 1
+}
+
+public static class CliUtils
+{
+    public static void SetExitCode(InvocationContext invCtx, PwpmExitCode exitCode)
+    {
+        invCtx.ExitCode = (int)exitCode;
+    }
+}
