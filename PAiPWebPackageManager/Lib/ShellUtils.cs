2ebe81edda62923d5cb263d4e83a7ff3bdc49582
Patryk Adamczyk <patrykadamczyk@patrykadamczyk.net>
2023-04-29 14:28:33 +0200

    ShellUtils Refactor

--- PAiPWebPackageManager/Lib/ShellUtils.cs
+++ PAiPWebPackageManager/Lib/ShellUtils.cs
@@ -4,39 +4,59 @@ namespace PAiPWebPackageManager.Lib;
 
 public static class ShellUtils
 {
+    /// <summary>
+    /// Check if current platform is Windows
+    /// </summary>
+    /// <returns>
+    /// True if current platform is Windows
+    /// </returns>
     public static bool IsWindows()
     {
         return RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
     }
     
+    /// <summary>
+    /// Check if current platform is Linux
+    /// </summary>
+    /// <returns>
+    /// True if current platform is Linux
+    /// </returns>
     public static bool IsLinux()
     {
         return RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
     }
     
+    /// <summary>
+    /// Check if current platform is Mac OS
+    /// </summary>
+    /// <returns>
+    /// True if current platform is Mac OS
+    /// </returns>
     public static bool IsMacOS()
     {
         return RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
     }
     
+    /// <summary>
+    /// Check if current platform is POSIX
+    /// </summary>
+    /// <returns>
+    /// True if current platform is POSIX
+    /// </returns>
     public static bool IsPosix()
     {
         return IsLinux()
                || IsMacOS()
                || RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD);
     }
-    
-    public static string GetExecutableExtension()
-    {
-        if (IsWindows())
-        {
-            return ".exe";
-        }
 
-        return "";
-    }
-    
-    public static string[] GetExecutableExtensions()
+    /// <summary>
+    /// Get Executable Extensions
+    /// </summary>
+    /// <returns>
+    /// Executable Extensions
+    /// </returns>
+    private static string[] GetExecutableExtensions()
     {
         if (IsWindows())
         {
@@ -46,6 +66,15 @@ public static class ShellUtils
         return new string[] { "" };
     }
 
+    /// <summary>
+    /// Expand Tilde to Home Directory
+    /// </summary>
+    /// <param name="path">
+    /// Path to expand
+    /// </param>
+    /// <returns>
+    /// Expanded path
+    /// </returns>
     public static string ExpandTilde(string path)
     {
         if (path.Length == 0 || !path.StartsWith("~")) return path;
@@ -64,18 +93,28 @@ public static class ShellUtils
         return path;
     }
 
-    public static string? FindExe(string exe)
+    /// <summary>
+    /// Find Executable
+    /// </summary>
+    /// <param name="executableToFind">
+    /// Executable to find
+    /// </param>
+    /// <returns>
+    /// Executable path if found
+    /// Null if not found
+    /// </returns>
+    public static string? FindExecutable(string executableToFind)
     {
-        if (exe.Length == 0) return null;
+        if (executableToFind.Length == 0) return null;
         var extensions = GetExecutableExtensions();
 
         string AddFileExt(string file, string extension) => Path.HasExtension(file) ? file : Path.ChangeExtension(file, extension);
         
-        if ((IsPosix() && exe.Contains('/')) || !IsPosix())
+        if ((IsPosix() && executableToFind.Contains('/')) || !IsPosix())
         {
             var foundInCurrentDir = extensions
                 .ToList()
-                .Select((ext) => AddFileExt(exe, ext))
+                .Select((ext) => AddFileExt(executableToFind, ext))
                 .Where(Path.Exists)
                 .Cast<string?>()
                 .FirstOrDefault(defaultValue: null);
@@ -90,34 +129,38 @@ public static class ShellUtils
         var candidatePaths = path.Split(Path.PathSeparator);
         if (candidatePaths.Length == 0) return null;
 
-        var candidates = candidatePaths
+        return candidatePaths
             .ToList()
             .Where((candidate) => candidate.Length != 0)
             .Select((candidate) => {
                 if (!IsWindows())
                 {
-                    return Path.Join(ExpandTilde(candidate), exe);
+                    return Path.Join(ExpandTilde(candidate), executableToFind);
                 }
 
                 if (candidate.StartsWith('"') && candidate.EndsWith('"'))
                 {
-                    return Path.Join(candidate.Substring(1, candidate.Length - 2), exe);
+                    return Path.Join(candidate.Substring(1, candidate.Length - 2), executableToFind);
                 }
 
-                return Path.Join(candidate, exe);
-            });
-
-        var realCandidates = candidates
+                return Path.Join(candidate, executableToFind);
+            })
             .SelectMany((candidate) => extensions.Select((ext) => AddFileExt(candidate, ext)))
             .Where(Path.Exists)
             .FirstOrDefault(defaultValue: null);
-
-        return realCandidates;
     }
     
-    
+    /// <summary>
+    /// Check if command is available
+    /// </summary>
+    /// <param name="command">
+    /// Command to check
+    /// </param>
+    /// <returns>
+    /// True if command is available
+    /// </returns>
     public static bool IsCommandAvailable(string command)
     {
-        return false;
+        return FindExecutable(command) is not null;
     }
 }
