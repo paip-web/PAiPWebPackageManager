ab825adc1b8ea1355e13e8e521b8a1c54046400f
Patryk Adamczyk <patrykadamczyk@patrykadamczyk.net>
2023-04-29 17:14:26 +0200

    Add Unhandled Error Exit Code

--- PAiPWebPackageManager/Lib/CliUtils.cs
+++ PAiPWebPackageManager/Lib/CliUtils.cs
@@ -4,14 +4,20 @@ namespace PAiPWebPackageManager.Lib;
 
 public enum PwpmExitCode
 {
+    UnhandledError = -1,
     Success = 0,
-    Failure = 1
+    Failure = 1,
 }
 
 public static class CliUtils
 {
+    public static int GetExitCode(PwpmExitCode exitCodeEnum)
+    {
+        return (int)exitCodeEnum;
+    }
+    
     public static void SetExitCode(InvocationContext invCtx, PwpmExitCode exitCode)
     {
-        invCtx.ExitCode = (int)exitCode;
+        invCtx.ExitCode = GetExitCode(exitCode);
     }
 }
